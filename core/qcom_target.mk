b5f414ac5dacd863bbb55715af0c0778583eeded
Steve Kondik <shade@chemlab.org>
2018-03-29 14:59:49 +0000

    build: Introduce project pathmap and use it for qcom variants
    The project pathmap tracks the path to the top level of a project.  The
    following functions are provided:
    
     * project-set-path adds an entry.
     * project-set-path-variant adds a "variant" entry.
     * project-path-for retrieves an entry.
    
    To use as a guard in Android.mk:
    
      ifeq ($(call my-dir),$(call project-path-for,projectname))
    
    To use for include paths in Android.mk:
    
      LOCAL_C_INCLUDES += $(call project-path-for,projectname)/...
    
    Set project pathmap for qcom project variants.
    
    Change-Id: I8dceca72a1ba80fc7b1830c5ab285d444f530457

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -3,17 +3,6 @@
 # Bring in Qualcomm helper macros
 include $(BUILD_SYSTEM)/qcom_utils.mk
 
-# Populate the qcom hardware variants in the project pathmap.
-define ril-set-path-variant
-$(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/$(1))
-endef
-define wlan-set-path-variant
-$(call project-set-path-variant,wlan,TARGET_WLAN_VARIANT,hardware/qcom/$(1))
-endef
-define bt-vendor-set-path-variant
-$(call project-set-path-variant,bt-vendor,TARGET_BT_VENDOR_VARIANT,hardware/qcom/$(1))
-endef
-
 # Set device-specific HALs into project pathmap
 define set-device-specific-path
 $(if $(USE_DEVICE_SPECIFIC_$(1)), \
@@ -87,19 +76,15 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     endif
     endif
     endif
+
+$(call set-device-specific-path,AUDIO,audio,hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT))
+$(call set-device-specific-path,DISPLAY,display,hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT))
+$(call set-device-specific-path,MEDIA,media,hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT))
+
+else
+
+$(call project-set-path,qcom-audio,hardware/qcom/audio/default)
+$(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
+$(call project-set-path,qcom-media,hardware/qcom/media/$(TARGET_BOARD_PLATFORM))
+
 endif
-
-$(call set-device-specific-path,AUDIO,audio,hardware/qcom/audio)
-$(call set-device-specific-path,DISPLAY,display,hardware/qcom/display)
-$(call set-device-specific-path,MEDIA,media,hardware/qcom/media)
-
-$(call set-device-specific-path,CAMERA,camera,hardware/qcom/camera)
-$(call set-device-specific-path,GPS,gps,hardware/qcom/gps)
-$(call set-device-specific-path,SENSORS,sensors,hardware/qcom/sensors)
-$(call set-device-specific-path,LOC_API,loc-api,vendor/qcom/opensource/location)
-$(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
-$(call set-device-specific-path,POWER,power,hardware/qcom/power)
-
-$(call ril-set-path-variant,ril)
-$(call wlan-set-path-variant,wlan)
-$(call bt-vendor-set-path-variant,bt)
